37a6f406dfd584d3648e9ee538b2ce9924545185
Taniya <tanya.kalra200@gmail.com>
2023-07-16 17:54:46 +0530

    submitting code-Sun Jul 16 2023 17:54:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a project", completed: false },
+    { id: 3, text: "Deploy to production", completed: false },
+  ]);
+
+  const handleComplete = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div className="parent">
+        <h1>Parent Component</h1>
+        <div className="child">
+          <h2>Child Component</h2>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.text} &nbsp;
+                {!todo.completed && (
+                  <button onClick={() => handleComplete(todo.id)}>
+                    Complete
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
